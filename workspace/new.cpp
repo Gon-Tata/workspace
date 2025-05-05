913636795f4eb27a7af87e107354922e8437e7ad
Gon_Tata <wzh20092021@outlook.com>
2025-05-05 14:04:21 +0800

    test

--- workspace/new.cpp
+++ workspace/new.cpp
@@ -1,31 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int inf=1e6+10;
-int t,n,m,fac[inf+2],p;
-int Pow(int a,int x){
-	int ans=1;
-	a%=p;
-	while(x){
-		if(x&1) ans=(1ll*ans*a)%p;
-		a=(1ll*a*a)%p;
-		x>>=1;
+const int mod=1e6+3;
+const int inf = 1e6 + 10;
+int t, n, m, fac[inf + 2], p;
+int Pow(int a, int x){
+	int ans = 1;
+	a %= p;
+	while (x){
+		if (x & 1)
+			ans = (1ll * ans * a) % p;
+		a = (1ll * a * a) % p;
+		x >>= 1;
 	}
 	return ans;
 }
-int C(int a,int b){
-	return a<b?0:1ll*fac[a]*Pow(fac[b],p-2)%p*Pow(fac[a-b],p-2)%p;
+int C(int a, int b){
+	return a < b ? 0 : 1ll * fac[a] * Pow(fac[b], p - 2) % p * Pow(fac[a - b], p - 2) % p;
 }
-int lucas(int a,int b){
-	return b?1ll*lucas(a/p,b/p)%p*C(a%p,b%p)%p:1;
+int lucas(int a, int b){
+	return b ? 1ll * lucas(a / p, b / p) % p * C(a % p, b % p) % p : 1;
 }
 int main(){
 	// freopen("a.in","r",stdin);
-	cin>>t;
-	while(t--){
-		scanf("%d%d%d",&n,&m,&p);
-		fac[0]=1;
-		for(int i=1;i<=p;++i) fac[i]=1ll*fac[i-1]*i%p;
-		printf("%d\n",lucas(n+m,n));
+	cin >> t;
+	while (t--){
+		scanf("%d%d%d", &n, &m, &p);
+		fac[0] = 1;
+		for (int i = 1; i <= p; ++i)
+			fac[i] = 1ll * fac[i - 1] * i % p;
+		printf("%d\n", lucas(n + m, n));
 	}
+
 	return 0;
 }
+/*
+
+*/
