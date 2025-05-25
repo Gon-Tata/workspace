cdb2d474fa7c68306e705593a4f9571d5ea60c4a
Gon_Tata <wzh20092021@outlook.com>
2025-05-25 17:44:19 +0800

    test

--- workspace/gg.cpp
+++ workspace/gg.cpp
@@ -1,88 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int inf=1e5+10;
-int qr,n,l,p;
-int q[inf],v[inf],nxt[inf];
-char s[inf][35];
-long double sum[inf],f[inf];
-bool vis[inf];
-void clear()
-{
-    memset(nxt, 0, sizeof(nxt));
-    memset(vis, false, sizeof(vis));
+const int inf=1e6+10;
+int t,n,k,ks,kl,ans=1,cnt,p[inf],c[inf];
+void clear(){
+	ans=1;
+	cnt=0;
+	memset(p,0,sizeof(p));
+	memset(c,0,sizeof(c));
 }
-
-long double qpow(long double a,int k){
-    long double ans=1;
-    while(k){
-        if(k&1) ans*=a;
-        a*=a;
-        k>>=1;
-    }
-    return ans;
-}
-long double val(int i,int j){return f[i]+qpow(abs(sum[j]-sum[i]-l-1),p);}
-
-int query(int p,int q){
-    int kl=max(p,q),kr=n,mid;
-    while(kl<kr){
-        mid=(kl+kr)>>1;
-        if(val(p,mid)<=val(q,mid)) kr=mid;
-        else kl=mid+1;
-    }
-    if(kr==n) return n+(val(p,n)>val(q,n));
-    return kr;
-}
-
-int main()
-{
-    freopen("a.in","r",stdin);
-    scanf("%d", &qr);
-    while (qr--)
-    {
-        // init();
-        // scanf("%d%d%d", &n, &l, &p);
-        // for (int i = 1; i <= n; i++)
-        // {
-        //     scanf("%s", s[i]);
-        //     sum[i] = sum[i - 1] + strlen(s[i]) + 1;
-        // }
-        cin>>n>>l>>p;
-        for(int i=1;i<=n;i++){
-            scanf("%s",s[i]);
-            sum[i]=sum[i-1]+strlen(s[i])+1;
-        }
-        int ql=1, qr=1;
-        // q[ql = qr = 1] = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            while ((ql < qr) && (v[ql + 1] <= i)) ql++;
-            f[i] = val(q[ql], i), nxt[i] = q[ql];
-            while ((ql < qr) && (v[qr] >= query(i, q[qr]))) qr--;
-            q[++qr] = i, v[qr] = query(i, q[qr - 1]);
-        }
-        // for(int i=1;i<=n;i++){
-        //     while(ql<qr&&v[ql+1]<=i) ql++;
-        //     f[i]=val(q[ql],i);
-        //     nxt[i]=q[ql];
-        //     while(ql<qr&&v[qr]>=query(i,q[qr])) qr--;
-        //     q[++qr]=i;
-        //     v[qr]=query(i,q[qr-1]);
-        // }
-        if (f[n] > 1e18) puts("Too hard to arrange");
-        else
-        {
-            printf("%lld\n", (long long)f[n]);
-            for (int i = n; i; i = nxt[i]) vis[i] = true;
-            int cur = 1;
-            while (cur <= n)
-            {
-                if (!vis[cur]) printf("%s ", s[cur]);
-                else printf("%s\n", s[cur]);
-                cur++;
-            }
-        }
-        puts("--------------------");
-    }
-    return 0;
+signed main(){
+	// freopen("a.in","r",stdin);
+	// freopen("a.out","w",stdout);
+	cin>>t;
+	for(int ki=1;ki<=t;ki++){
+		scanf("%d",&n);
+		for(int i=2;i<=sqrt(n);i++){
+			if(n%i==0){
+				p[++cnt]=i;
+				while(n%i==0) n/=i,c[cnt]++;
+			}
+		}
+		if(n>1){
+			p[++cnt]=n;
+			c[cnt]=1;
+		}
+		for(int i=1;i<=cnt;i++) ans*=(2*c[i]+1);
+		cout<<"Case "<<ki<<": "<<(ans/2+1)<<'\n';
+		clear();
+	}
 }
--- workspace/new.cpp
+++ workspace/new.cpp
@@ -1,11 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int inf=1e6+10;
-const int p=1e9+7;
-int n,m,fac[inf+2];
+const int inf=1e7+10;
+const int mood=999911659;
+int n,g;
+int fac[5][inf],mo[]={0,2,3,4679,35617,999911659},opt,a[5],ans,kmod;
+vector<int> facts;
+int get_fac(int x){return fac[opt][x]?fac[opt][x]:fac[opt][x]=get_fac(x-1)*x%mo[opt];}
 int Pow(int a,int x){
-	int ans=1;
+	// return 0;
+	int ans=1,p=mo[opt];
+	// cout<<p<<'\n';
 	a%=p;
 	while(x){
 		if(x&1) ans=(1ll*ans*a)%p;
@@ -14,18 +19,71 @@ int Pow(int a,int x){
 	}
 	return ans;
 }
-int C(int a,int b){
-    return a<b?0:1ll*fac[a]*Pow(fac[b],p-2)%p*Pow(fac[a-b],p-2)%p;
+int C(int n,int m){
+	int p=mo[opt];
+	return n<m?0:1ll*get_fac(n)*Pow(get_fac(m),p-2)%p*Pow(get_fac(n-m),p-2)%p;
 }
-int lucas(int a,int b){
-    return b?1ll*lucas(a/p,b/p)%p*C(a%p,b%p)%p:1;
+int lucas(int n,int m){
+	// return 0;
+	int p=mo[opt];
+    return m?1ll*lucas(n/p,m/p)%p*C(n%p,m%p)%p:1;
 }
-int main(){
-	fac[0]=1;
-	for(int i=1;i<=p;++i) fac[i]=1ll*fac[i-1]*i%p;
-	cin>>n>>m;
-
+int exgcd(int a,int b,int &x,int &y){
+	if(b==0){
+		x=1,y=0;
+		return a;
+	}
+	int d=exgcd(b,a%b,x,y);
+	int z=x;
+	x=y;
+	y=z-y*(a/b);
+	return d;
 }
-/*
-
-*/
+int inv(int a,int mod){
+	int x,y;
+	exgcd(a,mod,x,y);
+	return (x%mod+mod)%mod;
+}
+int crt(int a[],int m[],int n){
+	// return 0;
+	// int mul=1;
+	// for(int i=1;i<=n;i++) mul*=m[i];
+	int mul=mood-1;
+	// cout<<mul<<endl;
+	int t,c,k=0;
+	for(int i=1;i<=n;i++){
+		t=mul/m[i];
+		c=inv(t,m[i]);
+		k=(k+a[i]*t%mul*c%mul)%mul;
+	}
+	return (k+mul)%mul;
+}
+void get_facts(){
+	for(int i=1;i<=sqrt(n);i++){
+		if(n%i==0){
+			facts.push_back(i);
+			if(i*i!=n) facts.push_back(n/i);
+		}
+	}
+}
+signed main(){
+	cin>>n>>g;
+	if(g==mood){
+		cout<<0;
+		return 0;
+	}
+	for(int i=1;i<=4;i++) fac[i][0]=1;
+	get_facts();
+	int len=facts.size();
+	for(int i=1;i<=4;i++){
+		kmod=mo[i];
+		opt=i;
+		for(int j=0;j<len;j++){
+			a[i]=((a[i]+lucas(n,facts[j]))%kmod+kmod)%kmod;
+		}
+	}
+	ans=crt(a,mo,4);
+	opt=5;
+	ans=Pow(g,ans);
+	cout<<ans<<'\n';
+}
